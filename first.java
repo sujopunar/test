634a400d5b5097429da5f6f17b2b64a4ccada9a8
sujopunar <69193608+sujopunar@users.noreply.github.com>
2021-05-23 21:38:03 +0500

    Create first.java
    tester class

---
+++ first.java
@@ -0,0 +1,16 @@
+public class first {
+ private String name;
+ private int age;
+  
+  
+ public first(String name, int age)
+ {
+   this.name = name;
+   this.age = age;
+ }
+  
+  public String getName()
+  {
+    return name;
+  }
+}
